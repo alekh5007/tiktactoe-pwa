3ac1100b2aade13272402c06485ce8588aac0d99
alekh kumar <alekh5007@gmail.com>
2020-08-10 13:18:35 +0530

    Updates

---
+++ precache-manifest.23b5b9021683dacb5d7113883d920b46.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc174e788736a58c8254981d82b97ca4",
+    "url": "/alekh5007/tiktactoe-pwa.git/index.html"
+  },
+  {
+    "revision": "4ce92c57fedba864a4c8",
+    "url": "/alekh5007/tiktactoe-pwa.git/static/css/main.c3c37415.chunk.css"
+  },
+  {
+    "revision": "c35602a6eb87114a9588",
+    "url": "/alekh5007/tiktactoe-pwa.git/static/js/2.827857ef.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/alekh5007/tiktactoe-pwa.git/static/js/2.827857ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ce92c57fedba864a4c8",
+    "url": "/alekh5007/tiktactoe-pwa.git/static/js/main.e976aeec.chunk.js"
+  },
+  {
+    "revision": "5ab7ff723476cd7e2229",
+    "url": "/alekh5007/tiktactoe-pwa.git/static/js/runtime-main.9d982e49.js"
+  }
+]);
